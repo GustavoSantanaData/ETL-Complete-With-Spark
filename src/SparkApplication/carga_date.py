6c20ab71ccbdab3ca473a167cb687dbe14cb4e84
Gustavo Santana <83434799+GustavoSantanaData@users.noreply.github.com>
2024-06-18 22:18:25 -0300

    Update carga_date.py
    adding function to integrate with utils.py

--- src/SparkApplication/carga_date.py
+++ src/SparkApplication/carga_date.py
@@ -149,3 +149,82 @@ array_intervals_between_start_end = calculate_allintervals_todefinequery(
 
 
 stores_amount_writing_in_lake = 1
+
+retry_attempts = 4
+updated_intervals = array_intervals_between_start_end.copy()
+for interval_index, interval in enumerate(array_intervals_between_start_end):
+    for attempt in range(retry_attempts):
+        try:
+            interval = updated_intervals[interval_index]
+            end_date_with_extra_day = interval[1] + timedelta(days=1)
+            run_database_load(
+                namespace_name,
+                dataset_name,
+                sega_url,
+                interval[0],
+                end_date_with_extra_day,
+                partition_column,
+                sega_user,
+                sega_pass,
+                filename,
+                storage_bucket_name,
+                remote_log_file,
+                ss,
+                type_of_process="date",
+            )
+
+            stores_amount_writing_in_lake += 1
+
+            end_date_query = interval[1]
+            break
+        except Exception as e:
+            print(f">>> [WARN] Error on attempt {attempt + 1}: {str(e)}")
+            if attempt == retry_attempts - 1:
+                mensage = ">>> [WARN] All attempts failed. Shutting down."
+                update_status(id_request, "failed")
+                update_logs(mensage, filename, storage_bucket_name, remote_log_file)
+                sys.exit(1)
+
+            elif stores_amount_writing_in_lake > 1:
+                df_raw = ss.read.parquet(
+                    environment=Environment.PRODUCTION,
+                    zone=Zone.WORK,
+                    namespace=namespace_name,
+                    dataset=f"{dataset_name}",
+                    cloud_environment=cloud_environment,
+                )
+
+                date_retry = df_raw.select(max(col(partition_column))).collect()[0][0]
+
+                start_date_query = datetime.combine(date_retry, datetime.min.time())
+
+                days_distance_interval_query = quantity_level(
+                    quantity, start_date_query, end_date_query
+                )
+                updated_intervals = calculate_allintervals_todefinequery(
+                    start_date_query,
+                    end_date_query,
+                    days_distance_interval_query,
+                    type_of_process="date",
+                )
+
+                stores_amount_writing_in_lake = 1
+
+                print(f">>> [INFO] Trying again from {date_retry}...")
+
+                time.sleep(60)
+                break
+            else:
+                mensage = ">>> [INFO] Trying again..."
+                update_logs(mensage, filename, storage_bucket_name, remote_log_file)
+                time.sleep(60)
+
+    intervals = updated_intervals
+
+    if stores_amount_writing_in_lake > len(intervals):
+        update_status(id_request, "success")
+        mensage = ">>> [INFO] Process finished successfully"
+        update_logs(mensage, filename, storage_bucket_name, remote_log_file)
+        break
+
+ss.spark.stop()
